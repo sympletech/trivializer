93d0729f193797f8d4fff78ce52a08c9d7302235
Daniel Lewis <dlewis@sympletech.com>
2012-07-28 18:49:54 -0700

    Deleting a bunch of crap

--- Trivilizer.PhoneUI/Trivilizer.PhoneUI/scripts/master.js
+++ Trivilizer.PhoneUI/Trivilizer.PhoneUI/scripts/master.js
@@ -1,13 +1,31 @@
 ﻿$(function () {
-
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
 });
+
+var onSuccess = function (position) {
+    console.log('Latitude: ' + position.coords.latitude + '\n' +
+          'Longitude: ' + position.coords.longitude + '\n' +
+          'Altitude: ' + position.coords.altitude + '\n' +
+          'Accuracy: ' + position.coords.accuracy + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+          'Heading: ' + position.coords.heading + '\n' +
+          'Speed: ' + position.coords.speed + '\n' +
+          'Timestamp: ' + new Date(position.timestamp) + '\n');
+};
+
+function onError(error) {
+    alert('code: ' + error.code + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+
+
+
 
 function LoadPages() {
     $('div[data-role="view"]').each(function () {
         var src = $(this).attr('data-src');
         var target = this;
-        console.log(src);
-        console.log(this);
         if (src != null) {
             $.get(src, function (data) {
                 $(target).html(data);
