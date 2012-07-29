fe8193a34c0ee3f6d217abab7b4b150ef18ea518
Daniel Lewis <dlewis@sympletech.com>
2012-07-29 11:18:42 -0700

    del

--- Trivilizer.PhoneUI.Droid/gen/com/atthackathon/trivilizer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.atthackathon.trivilizer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Trivilizer.PhoneUI/Trivilizer.PhoneUI/scripts/master.js
+++ Trivilizer.PhoneUI/Trivilizer.PhoneUI/scripts/master.js
@@ -1,26 +1,21 @@
 ﻿$(function () {
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    InitFindGamePage();
 });
 
-var onSuccess = function (position) {
-    console.log('Latitude: ' + position.coords.latitude + '\n' +
-          'Longitude: ' + position.coords.longitude + '\n' +
-          'Altitude: ' + position.coords.altitude + '\n' +
-          'Accuracy: ' + position.coords.accuracy + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
-          'Heading: ' + position.coords.heading + '\n' +
-          'Speed: ' + position.coords.speed + '\n' +
-          'Timestamp: ' + new Date(position.timestamp) + '\n');
-};
 
-function onError(error) {
-    alert('code: ' + error.code + '\n' +
-          'message: ' + error.message + '\n');
+//****************************
+//      Utulities 
+//****************************
+function ReadQueryString(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    if (results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
-
-
-
-
 
 function LoadPages() {
     $('div[data-role="view"]').each(function () {
@@ -32,4 +27,53 @@ function LoadPages() {
             });
         }
     });
+}
+
+function GetLocation() {
+    navigator.geolocation.getCurrentPosition(LocationSuccess, LocationError);
+}
+
+var LocationSuccess = function (position) {
+    console.log('Latitude: ' + position.coords.latitude + '\n' +
+          'Longitude: ' + position.coords.longitude + '\n' +
+          'Altitude: ' + position.coords.altitude + '\n' +
+          'Accuracy: ' + position.coords.accuracy + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+          'Heading: ' + position.coords.heading + '\n' +
+          'Speed: ' + position.coords.speed + '\n' +
+          'Timestamp: ' + new Date(position.timestamp) + '\n');
+};
+
+function LocationError(error) {
+    alert('code: ' + error.code + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+//****************************
+//      Find A Game Page 
+//****************************
+function InitFindGamePage() {
+
+}
+
+function GetNearbyGames() {
+    var game = {
+        Id: 1,
+        Name: 'Test Game',
+        Description: 'This is a test game'
+    };
+
+    var games = [];
+
+    for (var i = 0; i < 3; i++) {
+        games.push(game);
+    }
+
+    for (var i in games) {
+        $('#nearby-games').append("<li data-icon='toprated'>" +
+            "<a class='game-link' data-game-id='" + games[i].Id + "' href='#tabstrip-ingame'>" +
+            "<div class='game-name'>" + games[i].Name + "</div>" +
+            "<div class=''>" + games[i].Description + "</div>" +
+            "</a></li>");
+    }
 }
